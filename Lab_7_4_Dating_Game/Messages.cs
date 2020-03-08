ec3cc1ee0b7e6491f35b981354ff44a449200cc6
PeterShipp <pshipp81@aol.com>
2020-03-08 12:26:04 -0700

    Add project files.

---
+++ Lab_7_4_Dating_Game/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_7_4_Dating_Game
+{
+    class Messages
+    {
+        public Dating_Profile sender;
+        public string messageTitle;
+        public string messageData;
+        public bool isRead;
+
+        public Messages(Dating_Profile sender, string messageTitle, string messageData)
+        {
+            this.sender = sender;
+            this.messageTitle = messageTitle;
+            this.messageData = messageData;
+            isRead = false;
+        }
+
+    }
+}
+
---
+++ Lab_7_4_Dating_Game/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab_7_4_Dating_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dating_Profile Donnell = new Dating_Profile("Donnell", "Love", 27, "Male");
+            Donnell.WriteBio("Strong outdoors type");
+
+            Dating_Profile Toya = new Dating_Profile("Godfearing", "Toya", 24, "Female");
+            Toya.WriteBio("New to this site");
+
+            Donnell.SendMessage("Hello Toya", "Want to get some coffee?", Toya);
+            Toya.ReadMessage();
+        }
+    }
+}
